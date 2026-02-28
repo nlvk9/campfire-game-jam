79910decd4f7b81b90fe2f91883f679783560a34
fox1669 <foxgundersenlord@gmail.com>
2026-02-28 17:03:24 -0500

    .

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-const _globals = {
-    config : require('./config.json')
+import { drawTiles } from './render'
+
+//todo: move globals to a more appropriate file
+var _globals = {
+    config : require('./config.json'),
+    camera : {x : 0, y : 0},
+    level : require('./levels/debug.json') //todo: implement levels
 }
 
+drawTiles(_globals)
--- render.js
+++ render.js
@@ -1,1 +1,7 @@
+export function drawTiles(globals)
+{
+    const canvas = document.getElementById(globals.canvas)
+    const level = globals.level
 
+
+}
